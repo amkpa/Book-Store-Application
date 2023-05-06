9d0f478c714011fbe4dbbbc9ddb28fbfb830da72
Amit <amitkumarpalai3@gmail.com>
2023-05-06 22:07:32 +0530

    properties - yml

--- src/main/java/com/book/Entity/Book.java
+++ src/main/java/com/book/Entity/Book.java
@@ -1,21 +1,21 @@
 package com.book.Entity;
 
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Entity
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Book {
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String author;
